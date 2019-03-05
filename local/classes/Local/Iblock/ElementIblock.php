cbe75d4ed31fa5dd256ba86244d4fd70938974dd
Bondar.Denys <bondar.denis.php@gmail.com>
2019-03-05 12:33:16 +0200

    add Class for getlist

---
+++ local/classes/Local/Iblock/ElementIblock.php
@@ -0,0 +1,73 @@
+<?php
+
+
+use \CIBlockElement;
+
+/**
+ * Вспомогательный класс для работы с элементами инфоблока
+ */
+class ElementIblock
+{
+	/**
+	 * Получение списка значений справочника
+	 *
+	 * @param string directory - код справочника по параметру arIblockId этого класса
+	 * @param array arAddOrder - Массив дополнительных сортировок
+	 * @param array arAddFilter - Массив дополнительных фильтров
+
+	 * @return array - массив значений
+	*/
+
+	public static function getList(string $directory, array $arAddOrder = null, array $arAddFilter = null, array $arAddSelect = null)
+	{
+		$iblockId = self::$arIblockId[$directory];
+		if (!$iblockId) {
+			if (self::LOG)
+				Log::add("Попытка получить несуществующий справочник", $directory, "AdsDirectory");
+			return false;
+		}
+
+		$arItems = array();
+
+		$cache = Cache::createInstance(); 
+		$key = "list_" . $iblockId . "_" . json_encode($arAddOrder) . "_" . json_encode($arAddFilter);
+		if ($cache->initCache(self::CACHE_TIME, $key, "/dev/AdsDirectory") && self::CACHE) { 
+			$arItems = $cache->getVars();
+		} elseif ($cache->startDataCache() || !self::CACHE) {
+			if (!Loader::includeModule("iblock")) {
+				$cache->abortDataCache();
+				return false;
+			}
+
+			$arOrder = array(
+				"NAME" => "ASC",
+				"ID" => "ASC",
+			);
+			if ($arAddOrder)
+				$arOrder = array_merge($arAddOrder, $arOrder);
+			else
+				$arOrder = array_merge(array("SORT" => "ASC"), $arOrder);
+
+			$arFilter = Array(
+				"IBLOCK_ID" => $iblockId,
+				"ACTIVE" => "Y",
+			);
+			if ($arAddFilter)
+				$arFilter = array_merge($arFilter, $arAddFilter);
+
+			$arSelect = array(
+				"ID"				
+			);
+			//Добавляем к селекту параметры по сео
+			$arSelect = array_merge($arSelect, $arAddSelect);
+			$res = CIBlockElement::GetList($arOrder, $arFilter, false, false, $arSelect);
+			while ($arFields = $res->GetNext()) {				
+				$arItems[] = $arFields;
+			}
+			
+			$cache->endDataCache($arItems); 
+		}
+
+		return $arItems;
+	}
+}
